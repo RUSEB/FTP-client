357a7b95eaf227b4cde7deeafbc1b14cabc3af13
Ruslan Gubaev <gub_ruslan@mail.ru>
2024-07-20 01:35:15 +0300

    Add handler query to ftp server(only passive mode now)

---
+++ src/main/java/ru/russeb/service/client/QueryHandler.java
@@ -0,0 +1,177 @@
+package ru.russeb.service.client;
+
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.TimeoutException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QueryHandler  implements AutoCloseable {
+    private static final String IP_SERVER_PATTERN = "\\(\\d+,\\d+,\\d+,\\d+,\\d+,\\d+\\)";
+
+    private static final String USER_COMMAND = "USER ";
+    private static final String PASSWORD_COMMAND = "PASS ";
+    private static final String PASSIVE_CONNECTION_COMMAND = "PASV";
+    private static final String QUIT_COMMAND = "QUIT";
+    private static final String SEND_POST_COMMAND = "PORT ";
+    private static final String RETRIEVE_FILE_COMMAND = "RETR ";
+    private static final String STORE_FILE_COMMAND ="STOR ";
+
+    private static final String LOGIN_SUCCESSFUL_CODE = "230";
+    private static final String SPECIFY_PASSWORD_CODE = "331";
+    private static final String ENTERING_PASSIVE_MODE_CODE = "227";
+    private static final String STARTING_DATA_TRANSFER_CODE = "150";
+    private static final String OPERATION_SUCCESSFUL_CODE = "226";
+
+    private final Socket manageSocket;
+    private Socket dataSocket;
+    private final BufferedReader commandIn;
+    private final PrintWriter commandOut;
+    private InputStream dataIn;
+    private OutputStream dataOut;
+    private TypeOfConnection typeOfConnection;
+
+    public QueryHandler(String host, int port) throws IOException {
+        manageSocket = new Socket(host, port);
+        commandIn = new BufferedReader(new InputStreamReader(manageSocket.getInputStream()));
+        commandOut = new PrintWriter(manageSocket.getOutputStream(), true);
+    }
+
+    public void login(String login, String password) throws IOException, TimeoutException {
+        sendCommand(USER_COMMAND + login);
+        waitingCommandResponse(SPECIFY_PASSWORD_CODE);
+        sendCommand(PASSWORD_COMMAND + password);
+        waitingCommandResponse(LOGIN_SUCCESSFUL_CODE);
+    }
+
+
+    public void chooseTypeOfConnection(TypeOfConnection typeOfConnection) throws IOException, TimeoutException {
+        this.typeOfConnection = typeOfConnection;
+    }
+
+    private void initDataChannel() throws IOException, TimeoutException {
+        switch (typeOfConnection){
+            case PASSIVE: passiveConnect();
+            case ACTIVE: activeConnect();
+        }
+    }
+
+    private void passiveConnect() throws IOException, TimeoutException {
+        sendCommand(PASSIVE_CONNECTION_COMMAND);
+        String line = waitingCommandResponse(ENTERING_PASSIVE_MODE_CODE);
+        dataSocket = new Socket(getHostFromParts(parseIp(line)),getPortFromParts(parseIp(line)));
+        dataIn = dataSocket.getInputStream();
+        dataOut = dataSocket.getOutputStream();
+    }
+
+    private static int[] parseIp(String responseServer){
+        int[] parts = new int[6];
+        Pattern regex = Pattern.compile(IP_SERVER_PATTERN);
+        Matcher matcher = regex.matcher(responseServer);
+        if(matcher.find()){
+            String[] partsStr = matcher.group().replace("(", "").replace(")", "").split(",");
+            parts = Arrays.stream(partsStr)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+        if (Arrays.stream(parts).allMatch(i -> i == 0)) {
+            throw new IllegalArgumentException("Неверный формат IP указан в ответе от сервера: " + responseServer);
+        }
+        return parts;
+    }
+
+    private static String getHostFromParts(int[] parts){
+        StringBuilder host = new StringBuilder();
+        for(int i = 0; i < 4; i++){
+            host.append(parts[i]).append(".");
+        }
+        host.deleteCharAt(host.length() - 1);
+        return host.toString();
+    }
+
+    private static int getPortFromParts(int[] parts){
+        return parts[4]*256+parts[5];
+    }
+
+
+
+    private void activeConnect(){
+        //        dataSocket = new Socket()
+        //        sendCommand(SEND_POST_COMMAND + );
+    }
+
+    private String waitingCommandResponse(String... expectedCodes) throws IOException, TimeoutException {
+        long startTime = System.currentTimeMillis();
+        long timeoutMs = 5000;
+        String line = "";
+        while (true){
+            if(commandIn.ready()){
+                line = commandIn.readLine();
+            }
+            if(Arrays.stream(expectedCodes).anyMatch(line::startsWith)){
+                break;
+            }
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (System.currentTimeMillis() - startTime > timeoutMs) {
+                throw new TimeoutException("Вышло время ожидания ответа от сервера!");
+            }
+
+        }
+        return line;
+    }
+
+    private void sendCommand(String command){
+        commandOut.println(command);
+        commandOut.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        sendCommand(QUIT_COMMAND);
+        commandIn.close();
+        commandOut.close();
+        manageSocket.close();
+        if (dataSocket != null) {
+            dataIn.close();
+            dataOut.close();
+            dataSocket.close();
+        }
+    }
+
+
+    public String readFile(String pathToFile) throws IOException, TimeoutException {
+        initDataChannel();
+        sendCommand(RETRIEVE_FILE_COMMAND +pathToFile);
+        waitingCommandResponse(STARTING_DATA_TRANSFER_CODE);
+        try (ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = dataIn.read(buffer)) != -1) {
+                byteArrayOut.write(buffer, 0, bytesRead);
+
+            }
+            waitingCommandResponse(OPERATION_SUCCESSFUL_CODE);
+            return byteArrayOut.toString();
+        }
+    }
+
+
+    public void writeFile(String pathToFile,String fileContent) throws IOException, TimeoutException {
+        initDataChannel();
+        sendCommand(STORE_FILE_COMMAND+pathToFile);
+        waitingCommandResponse(STARTING_DATA_TRANSFER_CODE);
+        try (OutputStreamWriter writer = new OutputStreamWriter(dataOut)) {
+            writer.write(fileContent);
+            writer.flush();
+        }
+        waitingCommandResponse(OPERATION_SUCCESSFUL_CODE);
+    }
+
+}
+
---
+++ src/main/java/ru/russeb/service/client/TypeOfConnection.java
@@ -0,0 +1,6 @@
+package ru.russeb.service.client;
+
+enum TypeOfConnection {
+    ACTIVE,
+    PASSIVE
+}
