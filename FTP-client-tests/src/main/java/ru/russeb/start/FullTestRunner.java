d3e10959522f6bd94eee1ad3d5320230a0e98129
Ruslan Gubaev <gub_ruslan@mail.ru>
2024-07-23 06:16:39 +0300

    fix path to files

--- FTP-client-tests/src/main/java/ru/russeb/start/FullTestRunner.java
+++ FTP-client-tests/src/main/java/ru/russeb/start/FullTestRunner.java
@@ -2,7 +2,6 @@ package ru.russeb.start;
 
 import org.testng.TestNG;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +9,7 @@ public class FullTestRunner {
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
         List<String> suiteFiles = new ArrayList<>();
-        File file = new File("src/main/resources/full-testng.xml");
-        suiteFiles.add(file.getAbsolutePath());
+        suiteFiles.add("../FTP-client-tests/src/main/resources/full-testng.xml");
         testNG.setTestSuites(suiteFiles);
         testNG.run();
     }
